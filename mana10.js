89a8a1ca07beb7f7cc75a0d7263e3fdffd1086fc
Manaball123 <manaball123@hotmail.com>
2021-11-21 11:07:01 +0800

    Update mana10.js

--- mana10.js
+++ mana10.js
@@ -39,41 +39,53 @@ UI.AddSubTab([ "Rage","SUBTAB_MGR","Custom Anti-Aim" ],"Custom Anti-Aim")
 
 /*Index explanations:
     AA[i]:preset number
-    AA[i][x]:mode(0=static,1=jitter,2=switch,3=sway,4=random)
+
+
 
         AA[i][0][x]:values for static
             0: real offset
             1: fake offset
             2: lby offset
+
+
         
         AA[i][1][x]:values for jitter
             0: real offset
             1: fake offset
             2: lby offset
+
             3: real jitter
             4: fake jitter
             5: lby jitter
-            6: real jitter delay
-            7: fake jitter delay 
-            8: lby jiter delay
-            9: real randomized intervals(1 or 0)
-            10: fake randomized intervals(1 or 0)
-            11: lby randomized intervals(1 or 0)
+
+            6: real randomized intervals(1 or 0)
+            7: fake randomized intervals(1 or 0)
+            8: lby randomized intervals(1 or 0)
+
+            9: real jitter delay
+            10: fake jitter delay 
+            11: lby jiter delay
+
             12: real jitter interval offset
             13. fake jitter interval offset
             14. lby jitter interval offset
+
+
         
         AA[i][2][x]: values for switch
             AA[i][2][0]: values for real offset
             AA[i][2][1]: values for fake offset
             AA[i][2][2]: values for lby offset
-            (delay[0]=time between 0--->1)
-            AA[i][2][3]: values for real offset switch delay
-            AA[i][2][4]: values for fake offset switch delay
-            AA[i][2][5]: values for lby offset switch delay
-
+            
             //calculated internally
-            AA[i][2][6]: values for max index of switch
+            AA[i][2][3]: values for max index of switch
+
+            (delay[0]=time between 0--->1)
+            AA[i][2][4]: values for real offset switch delay
+            AA[i][2][5]: values for fake offset switch delay
+            AA[i][2][6]: values for lby offset switch delay
+
+            
 
         AA[i][3][x]: values for sway
             0: real start
@@ -82,16 +94,19 @@ UI.AddSubTab([ "Rage","SUBTAB_MGR","Custom Anti-Aim" ],"Custom Anti-Aim")
             3: fake end
             4: lby start
             5: lby end
-           
-            6: real sway time
-            7: fake sway time
-            8: lby sway time
-            
+
             //calculated internally
-            9: real delta
-            10: fake delta
-            11: lby delta
+            6: real delta
+            7: fake delta
+            8: lby delta
         
+           
+            9: real sway time
+            10: fake sway time
+            11: lby sway time
+            
+
+            
         AA[i][4][x]: values for random
             0: real offset
             1: fake offset
@@ -99,104 +114,50 @@ UI.AddSubTab([ "Rage","SUBTAB_MGR","Custom Anti-Aim" ],"Custom Anti-Aim")
             3: real delta
             4: fake delta
             5: lby delta
-            6: real delay
-            7: fake delay
-            8: lby delay
-            9: real randomized intervals(1 or 0)
-            10: fake randomized intervals(1 or 0)
-            11: lby randomized intervals(1 or 0)
+
+            6: real randomized intervals(1 or 0)
+            7: fake randomized intervals(1 or 0)
+            8: lby randomized intervals(1 or 0)
+
+            9: real delay
+            10: fake delay
+            11: lby delay
+
             12: real randomize interval offset
             13. fake randomize interval offset
             14. lby randomize interval offset
     
-    AA[i][5][x]: active mode(0=static,1=jitter,2=switch,3=sway,4=random) for real(0),fake(1), and lby(2)
-    AA[i][6]: name of aa
+
+
+        AA[i][5][x]: active mode(0=static,1=jitter,2=switch,3=sway,4=random) for real(0),fake(1), and lby(2)
+
+
+
+        AA[i][6]: name of aa preset
 */
 var presetTemplate
 [
     [0,0,0], //static
-    [0,0,0,0,0,0,1.0,1.0,1.0,0,1.0,1.0,1.0], //jitter
+    [0,0,0,0,0,0,0,0,0,1.0,1.0,1.0,1.0,1.0,1.0], //jitter
     [
         //arrays for switch, can potentially be changed
         [0,0],//real
         [0,0],//fake
         [0,0],//lby
+        [1,1,1],//real,fake,lby max index of phase
         [1.0,1.0],//real delay
         [1.0,1.0],//fake delay
-        [1.0,1.0],//lby delay
-        [1,1,1]//real,fake,lby max index of phase
+        [1.0,1.0]//lby delay
+        
     ],
-    [0,0,0,0,0,0,1.0,1.0,1.0,0,0,0], //sway
-    [0,0,0,0,0,0,1.0,1.0,1.0,0,1.0,1.0,1.0], //random
-    0,
+    [0,0,0,0,0,0,0,0,0,1.0,1.0,1.0], //sway
+    [0,0,0,0,0,0,0,0,0,1.0,1.0,1.0,1.0,1.0,1.0], //random
+    [0,0,0],
     "Mana Default AA"
 ]
 var AA=[presetTemplate]
 
 var RAGEBOT=[]
-
-function dataToKey(type)
-{
-    switch(type)
-    {
-        //aa settings
-        case 0:
-            var indStr1="AA_";
-
-            //iterate through every preset
-            for(i=0;i<AA.length;i++)
-            {
-
-                var indStr2=toString(i)+"_"
-                
-                //static
-                for(j=0;j<AA[i][0].length;j++)
-                {
-                    var indStr3=toString(j)
-
-                    DataFile.SetKey(configName,AA[i][0][j],indStr1+indStr2+indStr3);
-                }
-
-                //jitter
-                for(k=0;k<AA[i][1].length;k++)
-                {
-                    var indStr3=toString(k)
-
-                    DataFile.SetKey(configName,AA[i][1][k],indStr1+indStr2+indStr3);
-                }
-
-                //switch
-                for(l=0;l<AA[i][2].length;l++)
-                {
-                    var indStr3=toString(l)+"_"
-                    for(m=0;m<AA[i][2][l];m++)
-                    {
-                        var indStr4=toString(m)
-                        DataFile.SetKey(configName,AA[i][2][l][m],indStr1+indStr2+indStr3+indStr4);
-                    }       
-                }
-                //sway
-                for(o=0;o<AA[i][3].length;o++)
-                {
-                    var indStr3=toString(o)
-
-                    DataFile.SetKey(configName,AA[i][3][o],indStr1+indStr2+indStr3);
-                }
-
-                //random
-                for(p=0;p<AA[i][4].length;p++)
-                {
-                    var indStr3=toString(p)
-
-                    DataFile.SetKey(configName,AA[i][4][p],indStr1+indStr2+indStr3);
-                }
-
-
-            }
-            break;
-        
-    }
-}
 
 function keyToData(type)
 {
@@ -213,48 +174,176 @@ function keyToData(type)
                 var indStr2=toString(i)+"_"
                 
                 //static
+                var indStr3="0_"
                 for(j=0;j<AA[i][0].length;j++)
                 {
-                    var indStr3=toString(j)
+                    var indStr4=toString(j)
 
-                    AA[i][0][j]=DataFile.GetKey(configName,indStr1+indStr2+indStr3);
+                    AA[i][0][j]=parseInt(DataFile.GetKey(configName,indStr1+indStr2+indStr3+indStr4));
                 }
 
                 //jitter
-                for(k=0;k<AA[i][1].length;k++)
+                var indStr3="1_"
+                for(j=0;j<AA[i][1].length;j++)
                 {
-                    var indStr3=toString(k)
-
-                    AA[i][1][k]=DataFile.GetKey(configName,indStr1+indStr2+indStr3);
+                    var indStr4=toString(j)
+                    
+                    if(j<=8)
+                    {
+                        AA[i][1][j]=parseInt(DataFile.GetKey(configName,indStr1+indStr2+indStr3+indStr4));
+                    }
+                    else
+                    {
+                        AA[i][1][j]=parseFloat(DataFile.GetKey(configName,indStr1+indStr2+indStr3+indStr4));
+                    }
                 }
 
                 //switch
-                for(l=0;l<AA[i][2].length;l++)
+                var indStr3="2_"
+                for(j=0;j<AA[i][2].length;j++)
                 {
-                    var indStr3=toString(l)+"_"
-                    for(m=0;m<AA[i][2][l];m++)
+                    var indStr4=toString(j)+"_"
+                    
+                    if(j<=3)
                     {
-                        var indStr4=toString(m)
-                        AA[i][2][l][m]=DataFile.GetKey(configName,indStr1+indStr2+indStr3+indStr4);
-                    }       
+                        for(k=0;k<AA[i][2][l];k++)
+                        {
+                        var indStr5=toString(k)
+                        AA[i][4][j]=parseInt(DataFile.GetKey(configName,indStr1+indStr2+indStr3+indStr4+indStr5));
+                        }   
+                    }
+                    else
+                    {
+                        for(k=0;k<AA[i][2][l];k++)
+                        {
+                        var indStr5=toString(k)
+                        AA[i][4][j]=parseFloat(DataFile.GetKey(configName,indStr1+indStr2+indStr3+indStr4+indStr5));
+                        }   
+                    }    
                 }
                 //sway
-                for(o=0;o<AA[i][3].length;o++)
+                var indStr3="3_"
+                for(j=0;j<AA[i][3].length;j++)
                 {
-                    var indStr3=toString(o)
+                    var indStr4=toString(j)
 
-                    AA[i][3][o]=DataFile.GetKey(configName,indStr1+indStr2+indStr3);
+                    if(j<=8)
+                    {
+                        AA[i][3][j]=parseInt(DataFile.GetKey(configName,indStr1+indStr2+indStr3+indStr4));
+                    }
+                    else
+                    {
+                        AA[i][3][j]=parseFloat(DataFile.GetKey(configName,indStr1+indStr2+indStr3+indStr4));
+                    }
                 }
 
                 //random
-                for(p=0;p<AA[i][4].length;p++)
+                var indStr3="4_"
+                for(j=0;j<AA[i][4].length;j++)
                 {
-                    var indStr3=toString(p)
+                    var indStr4=toString(j)
+                    if(j<=8)
+                    {
+                        AA[i][4][j]=parseInt(DataFile.GetKey(configName,indStr1+indStr2+indStr3+indStr4));
+                    }
+                    else
+                    {
+                        AA[i][4][j]=parseFloat(DataFile.GetKey(configName,indStr1+indStr2+indStr3+indStr4));
+                    }
+                    
+                }
+                //modes
+                var indStr3="5_"
+                for(j=0;j<AA[i][5].length;j++)
+                {
+                    var indStr4=toString(j)
 
-                    AA[i][4][p]=DataFile.GetKey(configName,indStr1+indStr2+indStr3);
+                    AA[i][5][j]=parseInt(DataFile.GetKey(configName,indStr1+indStr2+indStr3+indStr4));
+                }
+                //name
+                var indStr3="6"
+
+                AA[i][6][0]=DataFile.GetKey(configName,indStr1+indStr2+indStr3)
+                
+            }
+            break;
+        
+    }
+}
+
+function dataToKey(type)
+{
+    switch(type)
+    {
+        //aa settings
+        case 0:
+            var indStr1="AA_";
+
+            //iterate through every preset
+            for(i=0;i<AA.length;i++)
+            {
+
+                var indStr2=toString(i)+"_"
+                
+                //static
+                var indStr3="0_"
+                for(j=0;j<AA[i][0].length;j++)
+                {
+                    var indStr4=toString(j)
+
+                    DataFile.SetKey(configName,indStr1+indStr2+indStr3+indStr4,toString(AA[i][0][j]));
                 }
 
+                //jitter
+                var indStr3="1_"
+                for(j=0;j<AA[i][1].length;j++)
+                {
+                    var indStr4=toString(j)
 
+                    DataFile.SetKey(configName,indStr1+indStr2+indStr3+indStr4,toString(AA[i][1][j]));
+                }
+
+                //switch
+                var indStr3="2_"
+                for(j=0;j<AA[i][2].length;j++)
+                {
+                    var indStr4=toString(j)+"_"
+                    for(k=0;k<AA[i][2][l];k++)
+                    {
+                        var indStr5=toString(k)
+                        DataFile.SetKey(configName,indStr1+indStr2+indStr3+indStr4+indStr5,toString(AA[i][2][j][k]));
+                    }       
+                }
+                //sway
+                var indStr3="3_"
+                for(j=0;j<AA[i][3].length;j++)
+                {
+                    var indStr4=toString(j)
+
+                    DataFile.SetKey(configName,indStr1+indStr2+indStr3+indStr4,toString(AA[i][3][j]));
+                }
+
+                //random
+                var indStr3="4_"
+                for(j=0;j<AA[i][4].length;j++)
+                {
+                    var indStr4=toString(j)
+
+                    DataFile.SetKey(configName,indStr1+indStr2+indStr3+indStr4,toString(AA[i][4][j]));
+                }
+                //modes
+                var indStr3="5_"
+                for(j=0;j<AA[i][5].length;j++)
+                {
+                    var indStr4=toString(j)
+
+                    DataFile.SetKey(configName,indStr1+indStr2+indStr3+indStr4,toString(AA[i][5][j]));
+                }
+                //name
+                var indStr3="6"
+
+                DataFile.SetKey(configName,indStr1+indStr2+indStr3,AA[i][6][0])
+                
             }
             break;
         
@@ -351,7 +440,7 @@ function updateAA(preset)
 {
     AntiAim.SetOverride(1)
     var currentTime=Globals.Realtime()
-    //real
+    //iterate through 3 angle types
     for(i=0;i<3;i++)
     {
 
@@ -367,12 +456,12 @@ function updateAA(preset)
             case 1:
 
                 //if time to change phase
-                if(currentTime>=jitterTimer[i]+AA[preset][1][i+6]+jitterTimeOffset[i])
+                if(currentTime>=jitterTimer[i]+AA[preset][1][i+9]+jitterTimeOffset[i])
                 {
                     
                     jitterTimer[i]=currentTime;
                     //generates offsets if randomized set to true
-                    if(AA[preset][1][9+i]==1)
+                    if(AA[preset][1][i+6]==1)
                     {
                         jitterTimeOffset[i]=zeroToNegOne(Math.round(Math.random()))*Math.random()*AA[preset][1][i+12];
                     }
@@ -397,12 +486,12 @@ function updateAA(preset)
             //switch
             case 2:
                 
-                let phases=(AA[preset][2][6][i])
+                
                 //if phase completed
-                if(currentTime>=jitterTimer[i]+AA[preset][2][i+3][i])
+                if(currentTime>=switchTimer[i]+AA[preset][2][i+4][i])
                 {
                     //if phase index maxed out
-                    if(switchPhaseCounter[i]>=phases)
+                    if(switchPhaseCounter[i]>=AA[preset][2][3][i])
                     {
                         switchPhaseCounter[i]=0;
                     }
@@ -420,13 +509,13 @@ function updateAA(preset)
 
             //sway
             case 3:
-                let swayCycleTimer=swayTimer[i]+AA[preset][3][i+6]
+                let swayCycleTimer=swayTimer[i]+AA[preset][3][i+9]
                 if(currentTime<=swayCycleTimer)
                 {
                     //delta=(finish-start)
                     //percentage=(fullCycleTime-currentTime)/timePerCycle
 
-                    SetOffset(AA[preset][3][i]+(AA[preset][3][i+9]*(swayCycleTimer/AA[preset][3][i+6])),i);
+                    SetOffset(AA[preset][3][i]+(AA[preset][3][i+9]*(swayCycleTimer/AA[preset][3][i+9])),i);
                 }
                 else
                 {
