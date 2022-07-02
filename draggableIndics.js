a5dc47acfc11f1c7634b0a5cfa47dc959d09adda
Manaball123 <manaball123@hotmail.com>
2022-07-02 23:39:36 +0800

    made it usable

--- draggableIndics.js
+++ draggableIndics.js
@@ -1,28 +1,38 @@
-const { UI, Input } = require("./onetap");
+//const { UI, Input } = require("./onetap");
 
-class stringIndicator
+function StringIndicator(initialString, initialColor, fontName, fontSize, initialX, initialY, hitboxW, hitboxH, centered)
 {
-    constructor(font,initialString,initialX,initialY,initialColor,hitboxW,hitboxH)
-    {
-        this.font = font
-        this.string = initialString;
-        this.x = initialX;
-        this.y = initialY;
-        this.color = initialColor;
-        this.hitboxW = hitboxW;
-        this.hitboxH = hitboxH;
-    };
+    this.screenSize = Render.GetScreenSize();
+    this.fontName = fontName;
+    this.fontSize = fontSize * (this.screenSize[0] / 2560);
+    this.string = initialString;
+    this.color = initialColor;
+    this.x = initialX == undefined ? 200 : initialX;
+    this.y = initialY == undefined ? 200 : initialY;
+    this.x = this.x * (this.screenSize[0] / 2560)
+    this.y = this.y * (this.screenSize[0] / 2560)
+    this.hitboxW = hitboxW == undefined ? 200 : hitboxW;
+    this.hitboxH = hitboxH == undefined ? 200 : hitboxH;
+    this.centered = centered == undefined ? 0 : 1;
+
+    //this.font = Render.GetFont(this.fontName, this.fontSize, false);
+    
     //if cursor is on it and m1 pressed or something
-    isSelected() 
+    this.IsSelected = function() 
     {
-        if(UI.IsMenuOpen()==true)
+        if(UI.IsMenuOpen())
         {
             //key for m1
-            if(Input.IsKeyPressed(01))
+            if(Input.IsKeyPressed(0x01))
             {
                 cursorPos = Input.GetCursorPosition()
-                if(cursorPos[0] <= (this.x + (hitboxW/2)) && cursorPos[0] >= (this.x - (hitboxW/2)) && cursorPos[1] <= (this.y + (hitboxH/2)) && cursorPos[1] >= (this.y + (hitboxH/2)))
+                cursorPos[0] -= (this.hitboxW / 2);
+                //I dont know why this works
+                //But trust me it does
+                //cursorPos[1] -= (this.hitboxH / 2);
+                if(cursorPos[0] >= this.x && cursorPos[0] <= this.x + this.hitboxW);
                 {
+                    if(cursorPos[1] >= this.y && cursorPos[1] <= this.y + this.hitboxH)
                     return true;
                 }
             }
@@ -30,18 +40,102 @@ class stringIndicator
         return false;
     }
 
-    move(pos)
+    this.Move = function(pos)
     {
-        this.x = pos[0]
-        this.y = pos[1]
+        this.x = pos[0];
+        this.y = pos[1];
     }
 
-    render()
+    this.Drag = function()
     {
-        Render.String(this.x,this.y,1,this.string,this.color,this.font)
+        if(this.IsSelected())
+        {
+            
+            cursorPos = Input.GetCursorPosition()
+            cursorPos[0] -= (this.hitboxW / 2);
+            cursorPos[1] -= (this.hitboxH / 2);
+            //Cheat.Print(cursorPos.toString())
+            this.Move(cursorPos)
+        }
+    }
+
+    this.Draw = function()
+    {   
+        Render.String(this.x, this.y, this.centered, this.string, this.color, this.font);
+    }
+    this.DrawHitbox = function()
+    {
+        //Cheat.Print("called")
+        Render.FilledRect(this.x,this.y,this.hitboxW,this.hitboxH, [255,255,255,128])
+    }
+    
+    this.GetHitboxSize = function()
+    {
+        this.screenSize = Render.GetScreenSize()
+        //The best estimate of the correct width
+        this.hitboxW = this.fontSize * this.string.length * 0.6;
+        this.hitboxH = this.fontSize;
+    }
+
+    this.UpdateFont = function(name, size)
+    {
+
+        this.fontName = fontName;
+
+
+        this.font = Render.GetFont(this.fontName, this.fontSize, false);
+
+
+    }
+
+    this.OnResChange = function()
+    {
+
+        var scale = Render.GetScreenSize()[0] / this.screenSize[0];
+        this.screenSize = Render.GetScreenSize()
+        this.y = this.y * scale;
+        this.x = this.x * scale;
+        this.fontSize = fontSize * scale;
+    }
+
+    this.OnDraw = function()
+    {
+        /*
+        if(Render.GetScreenSize()[0] != this.screenSize[0])
+        {
+            this.OnResChange()
+        }
+        */
+        if(this.font == undefined)
+        {
+            this.UpdateFont()
+        }
+        
+        this.GetHitboxSize()
+        this.Drag()
+        this.Draw()
+        if(UI.IsMenuOpen())
+        {
+            this.DrawHitbox()
+        }
+        
     }
 
 }
+
+
+var str1 = new StringIndicator("AAAAAAA", [255,255,255,255], "calibri.ttf", 64);
+
+function OnDraw()
+{
+    str1.OnDraw()
+
+}
+
+Cheat.RegisterCallback("Draw", "OnDraw")
+
+
+
 
 
 
