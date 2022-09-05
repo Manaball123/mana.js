f0d260ebdbdb7aa23abe4b157020a8ce64e2c478
Manaball123 <mana123__@hotmail.com>
2022-09-05 20:42:54 +0800

    Update better_datafile.js

--- better_datafile.js
+++ better_datafile.js
@@ -4,11 +4,11 @@ function CopyObj(object)
 {
     return JSON.parse(JSON.stringify(object))
 }
-function SaveFromPath(fname, path, objectName, object)
+
+DataFile.PathToKey(path, startName)
 {
-    var key = objectName;
-    var len = path.length;
-   //Cheat.Print("\n Processing path " + path.toString())
+    var key = ""
+    
     for(var i = 0; i < len; i++)
     {
         
@@ -24,9 +24,19 @@ function SaveFromPath(fname, path, objectName, object)
         
 
     }
+}
+//Saves a singe key/value
+function SaveFromPath(fname, path, objectName, object)
+{
+    var key = objectName;
+    var len = path.length;
+   //Cheat.Print("\n Processing path " + path.toString())
+
     //Cheat.Print("\n Saving " + JSON.stringify(object) + " @ key " + key)
     DataFile.SetKey(fname, key, JSON.stringify(object))
 }
+
+//Loads a single key
 function LoadFromPath(fname, path, objectName, root_object)
 {
     var key = objectName;
@@ -54,52 +64,7 @@ function LoadFromPath(fname, path, objectName, root_object)
     obj[path[len - 1]] = JSON.parse(DataFile.GetKey(fname, key))
 }
 
-function NavigateDataFile(fname, root_object, object, path, objectName, type)
-{
-    //Cheat.Print("\n path is currently: " + path.toString())
-    //If its an array
-    if(Array.isArray(object))
-    {
-        var len = object.length;
-        for(var i = 0; i < len; i++)
-        {
-            var newPath = CopyObj(path);
-            newPath.push(i)
-            //Cheat.Print("\nLoading path " + newPath.toString())
-            NavigateDataFile(fname, root_object, object[i], newPath, objectName, type);
-        }
-    }
-    //If its not an array
-    else
-    {
-        //It could be an object
-        if(typeof(object) === "object" && object != null)
-        {
-            Object.keys(object).forEach(function(key)
-            {
-                var newPath = CopyObj(path);
-                newPath.push(key)
-                //Cheat.Print("\nLoading path " + newPath.toString())
-                NavigateDataFile(fname, root_object, object[key], newPath, objectName, type);
-                
-            })
-        }
-        //If not array or object 
-        else{
-            //If saving
-            if(type == 0)
-            {
-                SaveFromPath(fname, path, objectName, object);
-            }
-            //If loading
-            else
-            {
-                LoadFromPath(fname, path, objectName, root_object);
-                //Cheat.Print("\nLoading object from path " + path.toString())
-            }
-        }
-    }
-}
+
 
 
 //Saves the object into the datafile cache
@@ -167,6 +132,122 @@ obj1 = {
             "boob" : "dood"
         }]
     }]
+}
+
+//Internal implementation, ignore
+DataFile.NavigateDataFile = function(fname, rootObject, object, path, objectName, type)
+{
+    //Cheat.Print("\n path is currently: " + path.toString())
+    //If its an array
+    if(Array.isArray(object))
+    {
+        var len = object.length;
+        for(var i = 0; i < len; i++)
+        {
+            var newPath = CopyObj(path);
+            newPath.push(i)
+            //Cheat.Print("\nLoading path " + newPath.toString())
+            NavigateDataFile(fname, rootObject, object[i], newPath, objectName, type);
+        }
+    }
+    //If its not an array
+    else
+    {
+        //It could be an object
+        if(typeof(object) === "object" && object != null)
+        {
+            Object.keys(object).forEach(function(key)
+            {
+                var newPath = CopyObj(path);
+                newPath.push(key)
+                //Cheat.Print("\nLoading path " + newPath.toString())
+                NavigateDataFile(fname, rootObject, object[key], newPath, objectName, type);
+                    
+            })
+        }
+        //If not array or object 
+        else{
+            //If saving
+            if(type == 0)
+            {
+                SaveFromPath(fname, path, objectName, object);
+            }
+            //If loading
+            else
+            {
+                LoadFromPath(fname, path, objectName, rootObject);
+                //Cheat.Print("\nLoading object from path " + path.toString())
+            }
+        }
+    }
+}
+//Internal implementation, ignore
+DataFile.RecursivePrintObject = function(obj)
+{
+    //If its an array
+    if(Array.isArray(obj))
+    {
+        var len = obj.length;
+        Cheat.Print("[");
+        for(var i = 0; i < len; i++)
+        {
+            RP(obj[i])
+            Cheat.Print(", ")
+        }
+        Cheat.Print("]");
+    }
+    //If its not an array
+    else
+    {
+        //It could be an object
+        if(typeof(obj) === "object" && obj != null)
+        {
+            Cheat.Print("{")
+            Object.keys(obj).forEach(function(key)
+            {
+                Cheat.Print( key + " : ");
+                RP(obj[key])
+                Cheat.Print(", ")
+            })
+            Cheat.Print("}")
+        }
+        //If not array or object 
+        else{
+            //Print directly
+            Cheat.Print(obj.toString());
+        }
+    }
+}
+
+
+
+
+DataFile.Config = function(configStruct)
+{
+    this.obj = configStruct;
+
+    //Recursively print out all members
+    this.RecPrint = function()
+    {
+        
+
+        DataFile.RecursivePrintObject(this.obj)
+        
+    }
+
+    this.Save(fname)
+    {
+
+    }
+    //Returns an object with all the keys set to values in config
+    this.Load(fname)
+    {
+
+    }
+
+
+    this.NavigateDataFile()
+    
 }
 
 Cheat.Print("obj before load: \n")
